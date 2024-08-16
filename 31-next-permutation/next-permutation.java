206390c469073492985333de8cba4516c2eb230a
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2024-08-16 22:08:42 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (55.94%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        // Find the first index 'i' such that nums[i] < nums[i + 1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            // Find the first index 'j' such that nums[j] > nums[i]
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            // Swap nums[i] and nums[j]
+            swap(nums, i, j);
+        }
+
+        // Reverse the subarray starting at i + 1
+        reverse(nums, i + 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+        
+    }
