ac4e1d046166ea8704af909acf5a9e4357b1313f
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2024-08-17 20:53:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (41.71%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -9,32 +9,30 @@ class Solution {
         int right = matrix[0].length - 1;
 
         while (top <= bottom && left <= right) {
-            // Traverse the top row from left to right
+           
             for (int i = left; i <= right; i++) {
                 list.add(matrix[top][i]);
             }
-            top++; // Move the top boundary down
+            top++; 
 
-            // Traverse the right column from top to bottom
+            
             for (int i = top; i <= bottom; i++) {
                 list.add(matrix[i][right]);
             }
-            right--; // Move the right boundary left
+            right--; 
 
-            if (top <= bottom) { // Check if there is still a bottom row to process
-                // Traverse the bottom row from right to left
+            if (top <= bottom) { 
                 for (int i = right; i >= left; i--) {
                     list.add(matrix[bottom][i]);
                 }
-                bottom--; // Move the bottom boundary up
+                bottom--; 
             }
 
-            if (left <= right) { // Check if there is still a left column to process
-                // Traverse the left column from bottom to top
+            if (left <= right) { 
                 for (int i = bottom; i >= top; i--) {
                     list.add(matrix[i][left]);
                 }
-                left++; // Move the left boundary right
+                left++; 
             }
         }
 
