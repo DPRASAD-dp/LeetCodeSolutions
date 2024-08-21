5c6792e16eab6d252fa2dbc0f518e2ee254ede21
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2024-08-21 21:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (24.51%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,49 +1,37 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int[] newarr = removeDuplicates(nums);
-        Arrays.sort(newarr);
         int start = 0;
-        int end = newarr.length - 1;
+        int end = nums.length - 1;
         
         while (start <= end) {
             int mid = start + (end - start) / 2;
             
             
-            if (newarr[mid] == target) {
+            if (nums[mid] == target) {
                 return true;
             }
             
             
-            if (newarr[start] <= newarr[mid]) { 
-                if (target >= newarr[start] && target < newarr[mid]) {
+            if (nums[start] < nums[mid]) { 
+                if (target >= nums[start] && target < nums[mid]) {
                     end = mid - 1; 
                 } else {
                     start = mid + 1; 
                 }
-            } else { 
-                if (target > newarr[mid] && target <= newarr[end]) {
+            } else if((nums[start] > nums[mid])) { 
+                if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
                 } else {
                     end = mid - 1; 
                 }
+            }
+                else{
+                start++;
             }
         }
         return false;
 
         
     }
-    public static int[] removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-        
-        int[] uniqueNums = new int[set.size()];
-        int index = 0;
-        for (int num : set) {
-            uniqueNums[index++] = num;
-        }
-        
-        return uniqueNums;
-    }
+
 }
