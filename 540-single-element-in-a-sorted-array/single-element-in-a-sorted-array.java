03f72bfb6b787b60124059d4d9cc1fd769190915
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2024-08-21 22:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (72.73%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,40 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans =0;
-        for(int i = 0;i<nums.length;i++){
-            ans = ans^nums[i];
+        if (nums.length == 1) {
+            return nums[0];
         }
-        return ans;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Boundary checks for accessing nums[mid-1] and nums[mid+1]
+            boolean isLeftValid = (mid - 1 >= 0);
+            boolean isRightValid = (mid + 1 < nums.length);
+
+            // Check if mid is the single non-duplicate element
+            if ((!isLeftValid || nums[mid] != nums[mid - 1]) && (!isRightValid || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+
+            // Determine which side to search
+            if (isRightValid && nums[mid] == nums[mid + 1]) {
+                if (mid % 2 == 0) {
+                    start = mid + 1;  // Move to the right half if mid is even
+                } else {
+                    end = mid - 1;    // Move to the left half if mid is odd
+                }
+            } else if (isLeftValid && nums[mid] == nums[mid - 1]) {
+                if (mid % 2 == 0) {
+                    end = mid - 1;    // Move to the left half if mid is even
+                } else {
+                    start = mid + 1;  // Move to the right half if mid is odd
+                }
+            }
+        }
+
+        return -1;  // This should not be reached if the input array is valid
     }
 }
