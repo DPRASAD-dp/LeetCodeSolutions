b79ad2b4229b67b0363e6635d35d0687f7c97717
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-02-28 09:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (8.47%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,31 +10,39 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-     ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy; 
-
+        ListNode ans = new ListNode(0);
+        ListNode dummy = ans;
         ListNode head1 = list1;
         ListNode head2 = list2;
-
-        
-        while (head1 != null && head2 != null) {
-            if (head1.val < head2.val) {
-                tail.next = head1;
+        while(head1 != null && head2 != null){
+            if(head1.val < head2.val){
+                ans.next = new ListNode(head1.val);
+                ans = ans.next;
                 head1 = head1.next;
-            } else {
-                tail.next = head2;
+            }
+            else{
+                ans.next = new ListNode(head2.val);
+                ans = ans.next;
+                head2 = head2.next;
+
+            }
+        }
+
+        if(head1 == null){
+            while(head2 != null){
+                ans.next= new ListNode(head2.val);
+                ans = ans.next;
                 head2 = head2.next;
             }
-            tail = tail.next; 
         }
-
+        if(head2 == null){
+            while(head1 != null){
+                ans.next = new ListNode(head1.val);
+                ans = ans.next;
+                head1 = head1.next;
+            }
+        }
+        return dummy.next;
         
-        if (head1 != null) {
-            tail.next = head1;
-        } else {
-            tail.next = head2;
-        }
-
-        return dummy.next; 
     }
 }
