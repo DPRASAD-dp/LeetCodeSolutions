9968826faf5c0ce0af6b6e0b63d90f44ab272112
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-03-02 11:31:04 +0530

    Time: 3 ms (73.94%) | Memory: 43.1 MB (47.68%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,23 +1,27 @@
-import java.util.Stack;
-
 class Solution {
     public int longestValidParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
-        stack.push(-1); // Base case to handle edge cases
-        int maxLength = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        stack.push(-1);
+        int maxlength = 0;
+        for(int i = 0;i<s.length();i++ ){
+            if(s.charAt(i) == '('){
                 stack.push(i);
-            } else {
+            }
+            else {
                 stack.pop();
-                if (stack.isEmpty()) {
-                    stack.push(i); // Update base index for next valid sequence
-                } else {
-                    maxLength = Math.max(maxLength, i - stack.peek());
+                if(stack.isEmpty()){
+                    stack.push(i);
                 }
+                else {
+                    maxlength = Math.max(maxlength, i - stack.peek());
+
+
+
+                }
+
             }
         }
-        return maxLength;
+        return maxlength;
+        
     }
 }
