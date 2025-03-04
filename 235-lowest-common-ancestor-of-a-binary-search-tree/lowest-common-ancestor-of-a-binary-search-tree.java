c4296636c1c4e7fd697a78bb43c2c397b2e48778
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-03-04 10:34:46 +0530

    Time: 5 ms (100.00%) | Memory: 45 MB (78.65%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,47 +10,27 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        if( p == root || q == root){
-            return root;
-        }
-
-        ArrayList<TreeNode> arr1 = new ArrayList<>();
-        ArrayList<TreeNode> arr2 = new ArrayList<>();
-
-        findpath(root,p,arr1);
-        findpath(root,q,arr2);
-
-        return findlca(arr1,arr2);
-     
+        return lca(root,p,q);   
     }
 
-    public void findpath(TreeNode node,TreeNode a,ArrayList<TreeNode>list){
-        if(node.val == a.val){
-            list.add(a);
-            return;
+    public TreeNode lca(TreeNode node,TreeNode p,TreeNode q){
+        if(p.val==node.val || q.val==node.val){
+            return node;
+        }
+        if(p.val < node.val && q.val > node.val){
+            return node;
+        }
+        if(p.val > node.val && q.val < node.val){
+            return node;
+        }
+        if(p.val < node.val && q.val < node.val){
+            return lca(node.left,p,q);
+        }
+        if(p.val > node.val && q.val > node.val){
+            return lca(node.right,p,q);
         }
 
-        if(node.val > a.val){
-            list.add(node);
-            findpath(node.left,a,list);
-        }
-        if(node.val < a.val){
-            list.add(node);
-            findpath(node.right,a,list);
-        }
+        return node;
 
-
-    }
-
-    public TreeNode findlca(ArrayList<TreeNode> list1,ArrayList<TreeNode> list2){
-        TreeNode tn = null;
-        int min = Integer.MAX_VALUE;
-        for(int i = 0;i<Math.min(list1.size(),list2.size());i++){
-            if(list1.get(i) == list2.get(i) && list1.get(i).val<min){ 
-                tn = list1.get(i);
-            }
-        }
-        return tn;
     }
 }
