27738006cf76b005a518808989230751528f26a1
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-03-05 12:51:57 +0530

    Time: 1 ms (100.00%) | Memory: 49.5 MB (60.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,39 @@
 class Solution {
+    private int rows; 
+    private int cols; 
+    private int island = 0; 
+
     public int numIslands(char[][] grid) {
-        int count = 0;
-        for(int i = 0;i<grid.length;i++){
-            for(int j = 0;j< grid[i].length;j++){
-                if(grid[i][j] == '1'){
-                    checkIsland(grid,i,j);
-                        count++;
-                    }
+        rows = grid.length;
+        cols = grid[0].length;
 
-                }
+        for(int r = 0; r < rows; r++) {
+            search(grid, r); 
+        }
+        return island; 
+    }
+
+    public void search(char[][] grid, int r) {
+        char[] fR = grid[r];
+
+        for(int c = 0; c < cols; c++) {
+            if(fR[c] == '1') { 
+                mark(grid, r, c); 
+                island++; 
             }
-            return count;
         }
+    }
 
-        
+    public void mark(char[][] grid, int r, int c) {
+        grid[r][c] = '*'; 
 
-     public void checkIsland(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0'; 
-
-        
-        checkIsland(grid, i + 1, j);
-        checkIsland(grid, i - 1, j);
-        checkIsland(grid, i, j + 1);
-        checkIsland(grid, i, j - 1);
+        if(r > 0 && grid[r - 1][c] == '1')
+            mark(grid, r - 1, c);
+        if(r + 1 < rows && grid[r + 1][c] == '1')
+            mark(grid, r + 1, c);
+        if(c > 0 && grid[r][c - 1] == '1')
+            mark(grid, r, c - 1);
+        if(c + 1 < cols && grid[r][c + 1] == '1')
+            mark(grid, r, c + 1);
     }
 }
