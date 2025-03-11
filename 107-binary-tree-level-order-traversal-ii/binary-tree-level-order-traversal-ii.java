acbad4c1f3e6ce831335bc6f54c61adc54f92677
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-03-11 15:15:09 +0530

    Time: 1 ms (93.58%) | Memory: 43 MB (50.22%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        if(root  == null){
+            return new ArrayList<>();
+        }
+
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> arr = new ArrayList<>();
+            for(int i= 0;i<n;i++){
+            TreeNode temp = queue.poll();
+            arr.add(temp.val);
+            if(temp.left != null){
+                queue.add(temp.left);
+            }
+            if(temp.right != null){
+                queue.add(temp.right);
+            }}
+            ans.add(arr);
+
+        }
+
+    Collections.reverse(ans);
+    return ans;
+
+        
+    }
+}
