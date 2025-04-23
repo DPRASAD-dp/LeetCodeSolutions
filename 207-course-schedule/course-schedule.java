f0af75376cdff3ea49f523a075c7d5004bcbb25a
DURGA PRASAD KAVALI <durga.22bce9586@vitapstudent.ac.in>
2025-04-23 10:18:22 +0530

    Time: 7 ms (49.76%) | Memory: 45.2 MB (77.10%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,47 +1,51 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] dependencies  = new int[numCourses];
+        int n = numCourses;
+         ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> arr = new LinkedList<>();
+        int[] indegree = new int[n + 1];
 
-        for(int  i = 0 ; i< numCourses ; i++){
+        // Step 1: Build the graph and calculate indegree
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(int[] course : prerequisites){
-            graph.get(course[1]).add(course[0]);
-            dependencies[course[0]]++;
-
+        for(int[] edge : prerequisites) {
+            int u = edge[1];
+            int d = edge[0];
+            graph.get(u).add(d);
+            indegree[d]++;
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        for(int j = 0;j<numCourses;j++){
-
-            if(dependencies[j] == 0){
-                queue.add(j);
+        // Step 2: Start BFS for nodes with indegree 0
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                arr.add(i);
             }
-
         }
 
-        int finalcomplete = 0;
-        while(!queue.isEmpty()){
-            int a = queue.poll();
-            finalcomplete++;
+        bfs(graph, arr, indegree, ans);
 
+        
+        return ans.size() == n;
+    }
 
-            for(int c : graph.get(a)){
-                dependencies[c]--;
-                if(dependencies[c]==0){
-                    queue.add(c);
+    // BFS method to perform the topological sort
+    public static void bfs(ArrayList<ArrayList<Integer>> graph, Queue<Integer> arr, int[] indegree, ArrayList<Integer> ans) {
+        while (!arr.isEmpty()) {
+            int a = arr.poll();
+            ans.add(a);
+
+            for (int neighbor : graph.get(a)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    arr.add(neighbor);
                 }
             }
+        }
 
-        }
-        if(finalcomplete == numCourses){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+        
     }
 }
