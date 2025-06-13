578e7087080a797d7d032a370891916036829d5a
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-13 11:17:11 +0530

    Time: 4 ms (73.77%) | Memory: 45.4 MB (11.12%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,39 +1,43 @@
+import java.util.*;
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n1 = word1.length();
-        int n2 = word2.length();
-        int[][] cache = new int[n1][n2];
-         for (int[] row: cache) {
-            Arrays.fill(row, -1);
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m][n];
+
+        for(int i = 0;i<m;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return helper(0,n1,word1,0,n2,word2,cache);
+        return edit_distance(0,0,word1,word2,m,n,dp);
     }
-    int helper(int index1,int n1,String word1,int index2,int n2,String word2,int[][] cache){
 
-        if(index1 == n1){
-            return n2-index2;
-        }
-        if(index2 == n2){
-            return n1-index1;
+    public int edit_distance(int i ,int j,String word1, String word2,int m , int n,int[][] dp){
+        if(i == m) return n - j; 
+        if(j == n) return m - i; 
+
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        if(cache[index1][index2] != -1){
-            return cache[index1][index2];
+        int insertdist = 0;
+        int deletedist = 0;
+        int replacedist = 0;
+
+        if(word1.charAt(i) == word2.charAt(j)){
+            return 0 + edit_distance(i+1,j+1,word1,word2,m,n,dp);
         }
-        int result = -1;
-        if(word1.charAt(index1) == word2.charAt(index2)){
-            result = helper(index1+1,n1,word1,index2+1,n2,word2,cache);
-        }
+
         else{
 
-            int choice1 = 1+ helper(index1+1,n1,word1,index2,n2,word2,cache);
-            int choice2 = 1+ helper(index1,n1,word1,index2+1,n2,word2,cache);
-            int choice3  = 1+ helper(index1+1,n1,word1,index2+1,n2,word2,cache);
-
-            result = Math.min(Math.min(choice1,choice2),choice3);
-
+        insertdist =  1 + edit_distance(i,j+1,word1,word2,m,n,dp);
+        deletedist = 1 + edit_distance(i+1,j,word1,word2,m,n,dp);
+        replacedist = 1+ edit_distance(i+1,j+1,word1,word2,m,n,dp);
         }
-        cache[index1][index2] = result;
-        return result;
+
+        dp[i][j] = Math.min(insertdist,Math.min(deletedist,replacedist));
+        return dp[i][j];
+
     }
+
 }
