d573be0a885b3431da40056b05f1434c68a88204
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-14 17:46:33 +0530

    Time: 4 ms (93.16%) | Memory: 44.1 MB (76.82%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,24 +1,44 @@
+import java.util.*;
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        for(int i = 1;i<n;i++){
-            for(int j= 0;j<i;j++){
-                if(nums[j] < nums[i] ){
-                    if(dp[j]+1>dp[i]){
-                        dp[i] = dp[j]+1;
-                    }
-                }
-            }
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(nums[0]);
+        for(int i =1;i<n;i++){
+        if(nums[i] > arr.get(arr.size()-1)){
+            arr.add(nums[i]);
         }
-        int maxi = 0;
+        else{
+            bin_search(i,nums,arr);
+        }
 
-        for(int i =0;i<n;i++){
-            if(dp[i] > maxi){
-                maxi = dp[i];
-            }
         }
-        return maxi;
+
+        return arr.size();
+        
     }
-}
+
+    public void bin_search(int i,int[] nums,ArrayList<Integer> arr){
+        
+        int start  = 0;
+        int end = arr.size()-1;
+
+        while(start<=end){
+            int mid = (end-start)/2 + start;
+            if(arr.get(mid) == nums[i]){
+                return;
+            }
+            else if(arr.get(mid) > nums[i]){
+                end = mid-1;
+                
+            }
+            else{
+                start = mid+1;
+            }
+
+            }
+            arr.set(start,nums[i]);
+            
+
+        }
+    }
