86886a7ea2ea8e4ab06252be979fcd466d085ec4
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-22 23:37:51 +0530

    Time: 8 ms (25.50%) | Memory: 46.5 MB (17.61%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        int ans   = 0;
-        for(int i = 0;i<n;i++){
-            ans = nums[i]^ans;
+        for(int i =0;i<=n-1;i+=2){
+            if(i == n-1){
+                return nums[n-1];
+            }
+            if(nums[i] != nums[i+1] ){
+                return nums[i];
+            }
+            
         }
-        return ans;
-        
+        return -1;
     }
 }
