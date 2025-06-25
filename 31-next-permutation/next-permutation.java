31efa4e8e76f43fe3b1333758822bbc106b87fd3
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-25 18:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (94.71%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,41 +1,54 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i = nums.length - 2;
-
-        
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            int j = nums.length - 1;
-
-            
-            while (nums[j] <= nums[i]) {
-                j--;
+        int n = nums.length;
+        int a =-1;
+        int b =-1;
+        for(int i =n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                a = i;
+                break;
             }
+        }
+        if(a== -1){
+            reverseSubarray(nums,0,n-1);
+            return;
+        }
+        for(int j =n-1;j>=0;j--){
+            if(nums[a]<nums[j]){
+                b=j;
+                break;
+            }
+        }
+        
+
+        swap(a,b,nums);
+        reverseSubarray(nums,a+1,n-1);
+       
+    }
+
+    public void swap(int a,int b,int[] nums){
+        int temp =nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    public static void reverseSubarray(int[] arr, int startIndex, int endIndex) {
+    
+        if (arr == null || startIndex < 0 || endIndex >= arr.length || startIndex >= endIndex) {
+            return; 
+        }
+
+        int left = startIndex;
+        int right = endIndex;
+
+        while (left < right) {
+            
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
 
             
-            swap(nums, i, j);
-        }
-
-        
-        reverse(nums, i + 1);
-    }
-
-    private static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private static void reverse(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+            left++;
+            right--;
         }
     }
-        
-    }
+}
