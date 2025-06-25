34d6bee4ba6b0fa45f76db6eba60e8be92f5efd8
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-25 22:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (87.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -8,41 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+import java.util.LinkedList;
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ans = new ListNode(0);
-        ListNode dummy = ans;
-        ListNode head1 = list1;
+        ListNode head1  = list1;
         ListNode head2 = list2;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
         while(head1 != null && head2 != null){
             if(head1.val < head2.val){
-                ans.next = new ListNode(head1.val);
-                ans = ans.next;
-                head1 = head1.next;
+               current.next = head1;
+               current = current.next;
+               head1 = head1.next;
+               
             }
-            else{
-                ans.next = new ListNode(head2.val);
-                ans = ans.next;
-                head2 = head2.next;
 
-            }
+            else if(head1.val >= head2.val){
+               current.next = head2;
+               current = current.next;
+               head2 = head2.next;
+        }}
+
+        while(head1 != null){
+            current.next = head1;
+            current = current.next;
+            head1 = head1.next;
         }
 
-        if(head1 == null){
-            while(head2 != null){
-                ans.next= new ListNode(head2.val);
-                ans = ans.next;
-                head2 = head2.next;
-            }
+        while(head2 != null){
+            current.next = head2;
+            current = current.next;
+            head2 = head2.next;
         }
-        if(head2 == null){
-            while(head1 != null){
-                ans.next = new ListNode(head1.val);
-                ans = ans.next;
-                head1 = head1.next;
-            }
-        }
+
         return dummy.next;
         
     }
+    
+    
 }
