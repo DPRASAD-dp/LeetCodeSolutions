df16e054ade5fc519562f17677989ed9943746b7
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-06-27 22:08:20 +0530

    Time: 1 ms (99.86%) | Memory: 48.3 MB (83.44%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -13,24 +13,51 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode temp1 = headA;
         ListNode temp2 = headB;
-        while(temp1 != temp2){
-            if(temp1 == null){
-                temp1 = headB;
-            }
-            else{
-                 temp1 = temp1.next;
-            }
-            if(temp2 == null){
-                temp2 = headA;
-            }
-            else{temp2 = temp2.next;
 
-
-            }
-            
-            
-
+        while(temp1 != null && temp2 != null){
+             temp1 = temp1.next;
+             temp2 = temp2.next;
         }
-        return temp1;
+        int count = 0;
+        int p1 = 0;
+        int p2 = 0;
+        while(temp1 != null){
+            temp1 = temp1.next;
+            p1 =1;
+            count++;
+        }
+        while(temp2 != null){
+            temp2 = temp2.next;
+            p2 = 1;
+            count++;
+        }
+
+        temp1 = headA;
+        temp2 = headB;
+
+        if(p1 == 1){
+            while(count!=0){
+                temp1 = temp1.next;
+                count--;
+            }
+        }
+        else if(p2 ==1){
+            while(count!=0){
+                temp2 = temp2.next;
+                count--;
+            }
+        }
+
+        while(temp1 != null && temp2 != null){
+            if(temp1 == temp2){
+                return temp1;
+            }
+             temp1 = temp1.next;
+             temp2 = temp2.next;
+        }
+
+        return null;
+
+
     }
 }
