62d29f6a0a335e41f43f290dcf6490bb0f29d014
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-09 09:55:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (88.71%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,21 +14,20 @@
  * }
  */
 class Solution {
-    int maxsum = Integer.MIN_VALUE;
+    int maxsum  = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
         helper(root);
-        return maxsum;   
+        return maxsum;
     }
-    int helper(TreeNode node){
+    public int helper(TreeNode node){
         if(node == null){
             return 0;
         }
-        int left = helper(node.left);
+        int left =helper(node.left);
         int right = helper(node.right);
-        left = Math.max(0,left);
-        right = Math.max(0,right);
-        int sum  = node.val+ left + right;
-        maxsum = Math.max(sum,maxsum);
-        return Math.max(left,right)+node.val;
+
+        maxsum = Math.max(maxsum,left+right+node.val);
+
+        return Math.max(Math.max(left,right)+node.val,0);
     }
 }
