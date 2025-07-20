16c45c15cd5bfcc7f39cdf312820d31b6b05d4f4
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-20 07:10:34 +0530

    Time: 2 ms (80.35%) | Memory: 46.2 MB (41.54%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        for(int i =0;i<n;i++){
-        if(nums[i] == 0 && i != n-1){
-            swapwith(nums,i,n);
-        }
-        }
-}
-
-    public void swapwith(int[] nums,int a,int n){
-        for(int i =a+1;i<n;i++){
+        int j = 0;
+        for(int i =0;i<nums.length;i++){
             if(nums[i] != 0){
-                nums[a] = nums[i];
-                nums[i] = 0;
-                return;
+                nums[j] = nums[i];
+                j++;
             }
+            else{
+                continue;
+            }
+        }
+        while(j<nums.length){
+            nums[j] = 0;
+            j++;
         }
     }
 }
