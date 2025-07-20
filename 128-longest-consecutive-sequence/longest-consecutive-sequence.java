ab0d53bfec8930e4134d84861abf063d78997411
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-20 10:31:51 +0530

    Time: 31 ms (58.78%) | Memory: 68 MB (15.88%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,64 +1,25 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int m = nums.length;
-        int count = 1;
-        int maxcount = 1;
-        Arrays.sort(nums);
-        int a = removeduplicates(nums,m);
         
-        for(int k =0;k<a-1;k++){
-            if(nums[k] == (nums[k+1]-1)){
+        int maxcount =0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+
+        for(int i : set){
+            if(!set.contains(i-1)){
+            int curr = i;
+            int count = 1;
+            while(set.contains(curr+1)){
+                curr++;
                 count++;
             }
-            
-            else{
-                
-                count = 1;
-            }
-            maxcount = Math.max(count,maxcount);
 
-
+            maxcount = Math.max(maxcount,count);
+        }
         }
         return maxcount;
-    }
-    public static int removeduplicates(int a[], int n)
-    {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        // creating another array for only storing
-        // the unique elements
-        int[] temp = new int[n];
-        int j = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] != a[i + 1]) {
-                temp[j++] = a[i];
-            }
-        }
-
-        // checking the last element present in temp[]
-        boolean check = true;
-        for (int i = 0; i < j; i++) {
-            if (temp[i] == a[n - 1]) {
-                check = false;
-                break;
-            }
-        }
-
-        if (check == true) {
-
-            temp[j++] = a[n - 1];
-        }
-        // Changing the original array
-        for (int i = 0; i < j; i++) {
-            a[i] = temp[i];
-        }
-
-        return j;
     }
 }
