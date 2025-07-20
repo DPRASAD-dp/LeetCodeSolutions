7de979f9d3a9af4b7d46be82e4e91e77fa4cb613
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-20 15:39:18 +0530

    Time: 2 ms (54.98%) | Memory: 45 MB (43.50%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,27 +1,21 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // Initialize variables to store the maximum and minimum product up to the current element.
-        int maxProduct = nums[0];
-        int minProduct = nums[0];
+        int minprod = nums[0];
+        int maxprod = nums[0];
         int result = nums[0];
 
-        // Traverse the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // If the current number is negative, swap maxProduct and minProduct
-            if (nums[i] < 0) {
-                int temp = maxProduct;
-                maxProduct = minProduct;
-                minProduct = temp;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<0){
+                int temp = minprod;
+                minprod = maxprod;
+                maxprod = temp;
             }
 
-            // Update maxProduct and minProduct
-            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
-            minProduct = Math.min(nums[i], minProduct * nums[i]);
+            minprod = Math.min(nums[i],nums[i]*minprod);
+            maxprod = Math.max(nums[i],nums[i]*maxprod);
 
-            // Update result with the maximum product found so far
-            result = Math.max(result, maxProduct);
+            result = Math.max(result,maxprod);
         }
-
         return result;
     }
 }
