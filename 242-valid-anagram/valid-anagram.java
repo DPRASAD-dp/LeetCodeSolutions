9d9c46cdb976ddc07f4c202faf21e56389e69cef
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-22 19:45:29 +0530

    Time: 3 ms (91.55%) | Memory: 43.3 MB (66.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,11 +2,16 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
         char[] arr1 = s.toCharArray();
-        Arrays.sort(arr1);
         char[] arr2 = t.toCharArray();
-        Arrays.sort(arr2);
+        int[] alpha = new int[26];
         for(int i=0;i<s.length();i++){
-            if(arr1[i] != arr2[i]){
+            alpha[arr1[i]-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            alpha[arr2[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(alpha[i]!=0){
                 return false;
             }
         }
