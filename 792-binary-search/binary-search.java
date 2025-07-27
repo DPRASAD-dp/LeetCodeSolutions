03216681e3869ec9d11da5eb3f385142236e5e9d
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-27 11:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (77.72%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,31 +2,32 @@ class Solution {
     public int search(int[] nums, int target) {
         if (nums.length == 0) return -1;
 
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
         int start = 0;
         int end = 1;
 
-        // Exponentially find the boundary
         while (end < nums.length && nums[end] < target) {
             start = end;
-            end = 2 * end;
+            end = 2*end;
         }
-
-        // Ensure end is within bounds
-        end = Math.min(end, nums.length - 1);
+        end  = Math.min(end,nums.length-1);
 
         return binSearch(nums, start, end, target);
     }
 
     public int binSearch(int[] nums, int start, int end, int target) {
-        while (start <= end) {
+        while (start <= end && end < nums.length) {
             int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
+            } else if (nums[mid] > target) {
                 end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
 
