1da13b5e40f8d7cf6a3636821bc453f84d3905ea
Kavali Durga Prasad <durga.22bce9586@vitapstudent.ac.in>
2025-07-28 12:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (10.35%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,65 +1,59 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        if (nums.length == 0) return false;
-        if (nums.length == 1) return nums[0] == target;
-
-        // Check if array is already sorted
-        if (nums[0] < nums[nums.length - 1]) {
-            return binsearch(0, nums.length - 1, nums, target) != -1;
-        }
+     if (nums.length == 0) return false;
+        if (nums.length == 1) return nums[0] == target ? true : false;
 
         int pivot = findPivot(nums);
 
-        if (pivot != -1 && nums[pivot] == target) {
-            return true;
-        }
+if (pivot != -1 && nums[pivot] == target) {
+    return true;
+}
 
-        if (pivot == -1) {
-            return binsearch(0, nums.length - 1, nums, target) != -1;
-        }
+if (pivot == -1) {
+    return binsearch(0, nums.length - 1, nums, target) != -1;
+}
 
-        if (target >= nums[0]) {
-            return binsearch(0, pivot, nums, target) != -1;
-        } else {
-            return binsearch(pivot + 1, nums.length - 1, nums, target) != -1;
-        }
+if (target >= nums[0]) {
+    return binsearch(0, pivot, nums, target) != -1;
+} else {
+    return binsearch(pivot + 1, nums.length - 1, nums, target) != -1;
+}
+
     }
 
     public int findPivot(int[] nums) {
-        int start = 0, end = nums.length - 1;
+    int start = 0, end = nums.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
 
-            // Check if mid is pivot
-            if (mid < nums.length - 1 && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                return mid - 1;
-            }
-
-            // Handle duplicates
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                // Shrink the bounds
-                if (start < nums.length - 1 && nums[start] > nums[start + 1]) return start;
-                start++;
-
-                if (end > 0 && nums[end - 1] > nums[end]) return end - 1;
-                end--;
-            }
-            // Left half is sorted
-            else if (nums[start] <= nums[mid]) {
-                start = mid + 1;
-            }
-            // Right half is sorted
-            else {
-                end = mid - 1;
-            }
+        // Check if mid is pivot
+        if (mid < nums.length - 1 && nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+        if (mid > 0 && nums[mid] < nums[mid - 1]) {
+            return mid - 1;
         }
 
-        return -1;
+        // Handle duplicates safely
+        if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
+            if (start < nums.length - 1 && nums[start] > nums[start + 1]) return start;
+            start++;
+            if (end > 0 && nums[end - 1] > nums[end]) return end - 1;
+            end--;
+            continue;
+        }
+
+        // Left side is sorted
+        if (nums[start] <= nums[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
+
+    return -1;
+}
 
     public int binsearch(int start, int end, int[] nums, int target) {
         while (start <= end) {
